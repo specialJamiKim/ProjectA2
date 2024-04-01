0d5d89a7d15c22a62b33e0c519316d8247f68f02
sanepark <xofyd0038@naver.com>
2024-04-01 11:44:57 +0900

    구글 로그인 임시 완료
    12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,16 +79,8 @@ dependencies {
 
     // Google Play services
     implementation ("com.google.gms:google-services:4.3.15")
-    implementation ("com.google.android.gms:play-services-auth:20.5.0")
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth-ktx")
-
-    // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+
 
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 
@@ -102,5 +94,12 @@ dependencies {
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
     implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
 
 }
--- app/src/main/java/com/example/projecta2/View/LoginActivity.kt
+++ app/src/main/java/com/example/projecta2/View/LoginActivity.kt
@@ -1,15 +1,25 @@
 package com.example.projecta2.View
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projecta2.R
 import com.example.projecta2.model.User
 import com.example.projecta2.util.DialogHelper
 import com.example.projecta2.util.RetrofitInstance
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,64 +28,113 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var login: Button
     private lateinit var btnJoinPage: Button
+    private lateinit var btnGoogleLogin: Button
     private lateinit var userEmailTextView: EditText
     private lateinit var userPwTextView: EditText
     private lateinit var email: String
     private lateinit var password: String
 
+    // 구글 로그인 관련
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            Log.d("login", "이미 로그인 되어있음")
+        } else {
+            Log.d("login not yet", "로그인 되어있지 않음")
+        }
+
+        // 구글 로그인 초기화
+        setResultSignUp()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail().requestProfile().build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // 뷰 바인딩
+        btnGoogleLogin = findViewById(R.id.btnGoogleLogin)
         btnJoinPage = findViewById(R.id.btnJoinPage)
         userEmailTextView = findViewById(R.id.userEmailTextView)
         userPwTextView = findViewById(R.id.userPwTextView)
         login = findViewById(R.id.login)
 
+        // 로그인 버튼 클릭 리스너
         login.setOnClickListener {
             email = userEmailTextView.text.toString()
             password = userPwTextView.text.toString()
             signIn(email, password)
         }
 
+        // 회원가입 페이지 이동
         btnJoinPage.setOnClickListener {
             val intent = Intent(applicationContext, JoinActivity::class.java)
             startActivity(intent)
         }
+
+        // 구글 로그인 버튼 클릭 리스너
+        btnGoogleLogin.setOnClickListener {
+            Log.d("GoogleLogin", "구글 로그인 버튼 클릭")
+            googleSignIn()
+        }
     }
 
+    // 구글 로그인
+    private fun googleSignIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    // 일반 로그인
     private fun signIn(email: String, password: String) {
         val userService = RetrofitInstance.userService
-
         userService.signIn(email, password).enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
                 if (response.isSuccessful) {
-                    val user = response.body()
-                    if (user != null) {
-                        // 받은 User 객체의 이메일과 이름을 로그에 출력
-                        Log.d(">>>>", "Email : ${user.email}, name : ${user.name} ${user.phoneNumber}, ${user.address}, 생일 > ${user.birthDate}")
-                        // 로그인에 성공했으므로 HomeActivity로 이동
+                    response.body()?.let { user ->
+                        Log.d("LoginSuccess", "Email: ${user.email}, Name: ${user.name}")
                         val intent = Intent(applicationContext, HomeActivity::class.java)
                         startActivity(intent)
-                        finish() // 현재 액티비티를 종료하여 뒤로가기 버튼을 눌렀을 때 다시 돌아오지 않도록 함
-                    } else {
-                        // 사용자 객체가 null인 경우 처리할 내용 추가
-                        Log.e("Response Error", "Received null user object")
+                        finish()
+                    } ?: run {
+                        Log.e("ResponseError", "Received null user object")
                     }
                 } else {
-                    // 응답이 실패한 경우 처리할 내용 추가 => 로그인 실패
-                    Log.e("Response Error", "Code: ${response.code()} , 서버 연결 실패")
-                    // 여기에 notification을 통한 메시지 표시
+                    Log.e("ResponseError", "Code: ${response.code()} , 서버 연결 실패")
                     DialogHelper.showMessageDialog(this@LoginActivity, "Login Fail", "없는 회원입니다.\n아이디와 비밀번호를 확인해주세요.")
-
                 }
             }
 
             override fun onFailure(call: Call<User>, t: Throwable) {
-                // 요청이 실패한 경우 처리할 내용 추가
-                Log.e("Request Failed", "Error: ${t.message}", t)
-
+                Log.e("RequestFailed", "Error: ${t.message}", t)
             }
         })
+    }
+
+    // 구글 로그인 결과 처리
+    private fun setResultSignUp() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    handleSignInResult(task)
+                }
+            }
+    }
+
+    // 구글 로그인 결과 핸들링
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Log.d("GoogleLoginSuccess", "이메일: ${account.email}, 성: ${account.familyName}, 이름: ${account.givenName}, 전체 이름: ${account.displayName}, 프로필 사진 URL: ${account.photoUrl}")
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        } catch (e: ApiException) {
+            Log.w("GoogleLoginFailure", "signInResult:실패 code=${e.statusCode}", e)
+        }
     }
 }
